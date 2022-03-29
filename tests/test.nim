07368092fc046f6acdad50837a5d27d25825b4b0
tandy <tandy1000@cock.li>
2022-03-29 22:56:57 +0100

    add more bits of hydrogen-view-sdk main.ts

--- tests/test.nim
+++ tests/test.nim
@@ -1,8 +1,7 @@
 import
-  ../src/hydrogen_view_sdk_nim,
+  ../src/hydrogen_view_sdk_nim/[classes, functions],
   ../src/utils,
-  std/[jsffi, dom, asyncjs],
-  nodejs/jsurl
+  std/[jsffi, dom, asyncjs]
 
 importFromModule("{ Platform, Client, LoadStatus, createNavigation, createRouter, RoomViewModel, TimelineView }", "hydrogen-view-sdk")
 
@@ -16,26 +15,32 @@ var assetPaths = js{
   downloadSandbox: downloadSandboxPath,
   worker: workerPath,
   olm: js{
-    wasm: olmWasmPath
-    legacyBundle: olmLegacyJsPath
+    wasm: olmWasmPath,
+    legacyBundle: olmLegacyJsPath,
     wasmBundle: olmJsPath
   }
 }
 
 importModule("hydrogen-view-sdk/style.css")
 
-proc main =
+proc main {.async.} =
   let
     app = document.querySelector("#app")
     assetPaths = newJsObject()
     config = newJsObject()
     platform = newPlatform(app, assetPaths, config)
-    # platform = newPlatform(app, assetPaths, config)
-  #   navigation = createNavigation()
-  # platform.setNavigation(navigation)
-  # const urlRouter = createRouter({
-  #   navigation: navigation,
-  #   history: platform.history
-  # })
+    navigation = createNavigation()
+  platform.setNavigation(navigation)
+  let urlRouter = createRouter(js{
+    navigation: navigation,
+    history: platform.history
+  })
+  urlRouter.attach()
+  let
+    client = newClient(platform)
+    loginOptions = client.queryLogin("matrix.org")
+  client.startWithLogin(loginOptions.password("USERNAME", "PASSWORD"))
+  client.loadStatus()
 
-main()
+
+discard main()
